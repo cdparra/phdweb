2d9bf2b2c2d76c4012eb0e050564bbe793e666cd
Cristhian Parra <cristhian@macondo.local>
2013-03-07 11:21:38 +0100

    iweb-based old personal phd website

---
+++ CV_files/CV.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.1 local-build-20100429
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('CV_files/CVMoz.css')
+Widget.onload();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
